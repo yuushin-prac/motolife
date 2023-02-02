3f6710872aed96460ed1485090b1fd5070afa6d9
yuushin-prac <yuushin.n999n@gmail.com>
2023-02-03 03:53:20 +0900

    add error messages when login

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
 
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true
 
   attachment :image
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, {presence: true, uniqueness: { case_sensitive: false }}
+
   has_one :profile, dependent: :destroy
   has_many :articles, dependent: :destroy
   delegate :name, to: :profile
