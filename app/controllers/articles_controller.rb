94bc1ada500e1d012836b293f4e93d182e55ecda
yuushin-prac <yuushin.n999n@gmail.com>
2023-02-05 09:23:04 +0900

    add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.includes(:user).order(created_at: :desc)
+    @articles = Article.includes(:user).order(created_at: :desc).page(params[:page]).per(9)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @articles = @user.articles.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def edit
